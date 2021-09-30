f2d344cc12621f7d65f3b42192daa3d7770375a2
Zechariah Te Waaka <68577865+Zectew33@users.noreply.github.com>
2021-09-30 16:01:57 +1300

    VR Toggle (Save Point)

---
+++ Assets/Scripts/XR Scripts/XR.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Management;
+
+public class XR : MonoBehaviour
+{
+    public Canvas CanvasObject;
+    public GameObject HUD;
+    private bool Vrbool;
+
+    void Start()
+    {
+        StopXR();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (Vrbool)
+            {
+                StopXR();
+                Vrbool = false;
+                HUD.SetActive(true);
+            }
+            else
+            {
+                StartCoroutine(StartXR());
+                Vrbool = true;
+                CanvasObject.GetComponent<Canvas>().enabled = false;
+                HUD.SetActive(false);
+            }
+        }
+
+    }
+
+    public IEnumerator StartXR()
+    {
+        Debug.Log("Initializing XR...");
+        yield return XRGeneralSettings.Instance.Manager.InitializeLoader();
+
+        if (XRGeneralSettings.Instance.Manager.activeLoader == null)
+        {
+            Debug.LogError("Initializing XR Failed. Check Editor or Player log for details.");
+        }
+        else
+        {
+            Debug.Log("Starting XR...");
+            XRGeneralSettings.Instance.Manager.StartSubsystems();
+            
+
+
+        }
+    }
+
+    public void StopXR()
+    {
+        Debug.Log("Stopping XR...");
+        XRGeneralSettings.Instance.Manager.StopSubsystems();
+        XRGeneralSettings.Instance.Manager.DeinitializeLoader();
+        Debug.Log("XR stopped completely.");
+    }
+}
